2b2c114b8f01091ae0c65a72aee898fe0c472e3e
Marcel Piotrowski (mfrszpiotro) <marcel.piotrowski5543@gmail.com>
2024-01-23 18:37:34 +0100

    feat: pretty printing comparing results

--- processing_app.py
+++ processing_app.py
@@ -3,15 +3,15 @@ from app.ollie_grade.grade import Grade
 import app.config as cfg
 import app.ollie_grade.tests.config as test_cfg
 import pandas as pd
-import os
+import json
 
 filepath = test_cfg.OK_OLLIE_TEST_FILE_PATH
 df = pd.read_csv(filepath)
-ollie_ok = Ollie(df, "okay")
+ollie_almost = Ollie(df, "almost-good")
 filepath = test_cfg.GOOD_OLLIE_TEST_FILE_PATH
 df = pd.read_csv(filepath)
-ollie_good = Ollie(df, "damn good")
+ollie_good = Ollie(df, "good")
 
-print(repr(ollie_good), repr(ollie_ok))
-comparator = Grade(ollie_ok, ollie_good)
-print(f"Comparing results: {comparator.compare()}")
+print(repr(ollie_good), repr(ollie_almost))
+comparator = Grade(ollie_almost, ollie_good)
+print(f"Comparing results: {json.dumps(comparator.compare(), indent=4)}")
